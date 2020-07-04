d8b6de9409d7ed5ecb603a2afb21220f3307e149
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-07-04 19:49:52 -0300

    atualizações ao backend

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -1,28 +1,8 @@
 const connection = require('../config/database')
-const trending = require('../utils/trending')
 const byTopic = require('../utils/byTopic.js')
 const newDate = require('../utils/newDate')
 
 module.exports = {
-  async index(req, res) {
-    const dateCurrent = newDate()
-    const data =
-      await connection('tweets')
-        .where('created_at', '=', dateCurrent)
-        .select('*')
-
-    const topTrending = trending(data)
-
-    const topTrendingSerialized = topTrending.map((trends) => {
-      return {
-        ...trends,
-        select: 0
-      }
-    })
-
-    return res.json(topTrendingSerialized)
-  },
-
   async show(req, res) {
     const { topic } = req.params
     const dateCurrent = newDate()
